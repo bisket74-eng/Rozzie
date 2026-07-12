0895abc6bc54ff064823272ac7e42e5c085adac8
bisket74-eng <bisket74@gmail.com>
2026-07-11 21:06:39 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'rozzies-playhouse-v1';
-const FILES = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
